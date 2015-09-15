fbad257c22ea92e294eb2de3f6ad259f9d0025ee
cipanur <cipa_nurani@yahoo.co.id>
2015-09-15 20:59:21 +0700

    infobunga

---
+++ js/slider.js
@@ -0,0 +1,33 @@
+
+	$(function () {
+	// Slideshow Home
+      $("#slider_home").responsiveSlides({
+        
+        pager: false,
+        nav: true,
+        speed: 500,
+        namespace: "callbacks",
+        before: function () {
+          $('.events').append("<li>before event fired.</li>");
+        },
+        after: function () {
+          $('.events').append("<li>after event fired.</li>");
+        }
+      });
+
+	  
+      // Slideshow Form Bus
+      $("#slider").responsiveSlides({
+        
+        pager: false,
+        nav: true,
+        speed: 500,
+        namespace: "callbacks",
+        before: function () {
+          $('.events').append("<li>before event fired.</li>");
+        },
+        after: function () {
+          $('.events').append("<li>after event fired.</li>");
+        }
+      });    
+      });
